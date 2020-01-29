b251fd55ee8328d59f30f8b6e7b50f55c7d7ded1
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-01-29 21:55:32 +0100

    added first private key pair

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs=require('fs');
+const bcoin =require('bcoin');
+
+const KeyRing =bcoin.wallet.WalletKey;
+const Script= bcoin.Script;
+
+
+// create 2 private keys
+const keypair1 =KeyRing.generate(true,'regtest');
+const keypair2 =KeyRing.generate(true,'regtest');
+
+fs.writeFileSync('regtest-key1.wif',keypair1.toSecret('regtest'));
+fs.writeFileSync('regtest-key2.wif',keypair2.toSecret('regtest'));
+
+const m = 2;
+const n = 2;
+
