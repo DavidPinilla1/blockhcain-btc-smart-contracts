e00773f80ef2ad729d4ac53e387670a2e1eec8af
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-02-08 17:57:37 +0100

    coin prepared with the reedem script created and the transaction info

--- transaction.js
+++ transaction.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const bcoin = require('bcoin');
 
 const KeyRing =bcoin.wallet.WalletKey;
-const {Script,MTX,Amount,Coin} = bcoin.MTX;
+const {Script,MTX,Amount,Coin} = bcoin;
 
 const privateKeys1 = fs.readFileSync('./regtest-key1.wif').toString();
 const privateKeys2 = fs.readFileSync('./regtest-key1.wif').toString();
@@ -14,12 +14,21 @@ const keyPair2 =KeyRing.fromSecret(privateKeys2)
 const publickey1 =keyPair1.publicKey;
 
 const publickey2 =keyPair2.publicKey;
-const m = 2;
-const n = 2;
 
 //Redeem Script
-const redeem =Script.fromMultisig(m, n, [publickey1,publickey2]);
+const redeem =Script.fromMultisig(2, 2, [publickey1,publickey2]);
 // Pay to script hash (P2SH)
 const script =Script.fromScripthash(redeem.hash160())
-console.log(script);
 
+const txInfo ={
+    value: Amount.fromBTC('100').toValue(),
+    hash:'3b1dd17cc82e2ac43ba62bf8f1c6a0fe805df43911653d22c902571eb3a212ce',
+    index:0
+}
+const coin =Coin.fromJSON({
+    ...txInfo,
+    version:1,
+    height:-1,
+    coinbase:false,
+    script:script.toJSON(),
+})
